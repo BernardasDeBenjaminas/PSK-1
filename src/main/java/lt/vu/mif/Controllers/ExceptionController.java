42ae4b7484a8c287a175ec30ef1392ace73fcd5e
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2018-05-02 14:20:55 +0300

    Add and use Foundation. Add Many-To-One relationship (Driver/Car). Add the ability to dynamically add new Driver/Car.

--- src/main/java/lt/vu/mif/Controllers/ExceptionController.java
+++ src/main/java/lt/vu/mif/Controllers/ExceptionController.java
@@ -18,17 +18,40 @@ public class ExceptionController {
         Car car = carRepository.get(1);
         Thread.sleep(5000);
         // CHANGE THESE VALUES TO NEW ONES IF YOU WANT THE EXCEPTION
-        car.setManufacturer("Hoo");
-        car.setModel("Hoo");
-        carRepository.update(car);
+        // 1. Kas nutinka su einamąja transakcija gavus klaidą?
+        //      - the transaction is marked for rollback;
+        // 2. Kas nutinka su einamuoju EntityManager, kai gauna klaida?
+        //      - managed instances become detached.
+        car.setModel("aqw");
+        carRepository.update(car); // Optimistic locking is used implicitly for writting
     }
 
     @Transactional()
     public void modifyCarButFaster() {
         Car car = carRepository.get(1);
         // CHANGE THESE VALUES TO NEW ONES IF YOU WANT THE EXCEPTION
-        car.setManufacturer("Test");
-        car.setModel("Test");
+        car.setModel("dsa");
+        carRepository.update(car);
+    }
+
+
+    public void modifyCarSafe() throws InterruptedException {
+        Car car = carRepository.get(1);
+        Thread.sleep(5000);
+        try {
+            car.setModel("Menuliukas3");
+            carRepository.update(car);
+        } catch (Exception e) {
+            // Kitas būdas su session
+            car = carRepository.get(1);
+            car.setModel("Menuliukas3");
+            carRepository.update(car);
+        }
+    }
+
+    public void modifyCarButFasterAndSafer() {
+        Car car = carRepository.get(1);
+        car.setModel("Debeselis3");
         carRepository.update(car);
     }
 }
